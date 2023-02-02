da145880e391556d16bc5a6fa2b3fc3234aa220e
Francesco <G00425679@atu.ie>
2023-02-02 21:22:57 +0000

    Week 02

---
+++ Week 02/exercise.py
@@ -0,0 +1,21 @@
+#prompt the user and read in two money amonts (in cent)
+#add the to amounts
+#print out the answer in a human readable format
+#with a euro sign and decimal point between the euro
+#and cent of the amount
+
+# print ((65+180)/100)
+
+amount = 65
+amount1 = 180
+
+#print (u"\N{euro sign}")
+
+#print (u"\u20ac")
+
+cen = float ((amount + amount1)/100)
+euro = u"\u20ac"
+
+#print (euro + str(cen))
+
+print("the sum of these is " + cen + f"{euro}
---
+++ Week 02/nameAndAge.py
@@ -0,0 +1,7 @@
+#name = input("Hello ")
+#age = input (", your age is ")
+#print ("name+ age.")
+
+name = "Franceso"
+age = "29"
+print ("Hello " + name +", \tyour age is " + age+".")

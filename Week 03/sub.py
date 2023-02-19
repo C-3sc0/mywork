a8da6c2feb5c73e7dec257749f5a68dd91250b87
Francesco <G00425679@atu.ie>
2023-02-19 10:29:59 +0000

    substract 2 numbers

---
+++ Week 03/sub.py
@@ -0,0 +1,7 @@
+#reads in two numbers and substracts
+#the first one from the second one
+
+a = int(input("Enter first Number:"))
+b = int(input("Enter second Number:"))
+c = a-b
+print (f'{a} minus {b} is {c}')

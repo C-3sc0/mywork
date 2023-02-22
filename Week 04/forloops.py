02604ae4e66b2ca6d877b706ac6a790901a1dcf0
Francesco <G00425679@atu.ie>
2023-02-22 19:48:54 +0000

    forloops

---
+++ Week 04/forloops.py
@@ -0,0 +1,12 @@
+
+
+
+boats = ["Sigma", "X yacth", "Swan"]
+
+for boat in boats:
+    print ("i'd rather be out in a", boat)
+
+for i in range (1,10):
+    print(i)
+
+print("all done i is now", i)

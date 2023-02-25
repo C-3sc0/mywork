cabbfaf4c0b43ae26b2480f78d885c0cae0dab5d
Francesco <G00425679@atu.ie>
2023-02-25 18:13:14 +0000

    quiz

---
+++ labs/topic05-datastructures/quiz.py
@@ -0,0 +1,20 @@
+number_of_Questions = 5 #int
+average_age = 23.4 #float
+debug_mode = True #boolean
+name = "Joe" #string
+ages = [] #array (get it wrong)
+months = ("jan", "Feb", "Mar") #tuples
+book = {} #str (get it wrong)
+stuff = [12, "Fred", False, {}] #dict, array X
+me  ={   "firstName" : "Andrew",
+    "teaching" : [{
+        "courseName" : "programming",
+        "semester" : 1
+    },{
+        "courseName" : "Data Representation",
+        "semester" : 2
+    }
+    ]
+}
+
+print(type(someone))

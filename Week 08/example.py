8ed0dbea7a57dc963947a2b6cf0d3c28a6cc2d36
Francesco <G00425679@atu.ie>
2023-03-16 12:20:38 +0000

    numpy examples

---
+++ Week 08/example.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+
+list_of_number = [1,2,3,5]
+
+print (list_of_number)
+
+numbers = np.array([1,2,3,4])
+print (numbers)
+
+
+
+random = np.random.randint(100,200,30)
+#30 numbers between 100 and 200
+print (random)
+
+normal = np.random.normal(size = 100)
+print (normal)
